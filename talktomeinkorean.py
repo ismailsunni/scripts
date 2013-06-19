8be094c5627f4acd02ff815a211d50f66f6828b7
Ismail Sunni <imajimatika@gmail.com>
2013-06-19 22:03:30 +0700

    Inital commit, download mp3 and pdf file.

---
+++ talktomeinkorean.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""A script to download material from talktomeinkorean.com.
+Please support them since they are really great.
+You can read the way to support them in
+http://www.talktomeinkorean.com/support/
+"""
+
+__author__ = 'Ismail Sunni'
+__copyright__ = 'Ismail Sunni'
+__license__ = 'GPL'
+__version__ = '0.0.1'
+__maintainer__ = 'Ismail Sunni'
+__email__ = 'imajimatika@gmail.com'
+__status__ = 'Prototype'
+__date__ = '19 June 2013'
+
+import urllib
+
+def download_audio(level, lesson, filename=None):
+    source_file = ('http://media.libsyn.com/media/talktomeinkorean/'
+                   'TTMIK-Lesson-L%sL%s.mp3') % (level, lesson)
+    
+    if filename is None:
+        filename = ('TTMIK-Lesson-L%sL%s.mp3') % (level, lesson)
+    
+    # Simple download file
+    # TO DO : Change to http://stackoverflow.com/a/9740603/1198772
+    print '[+] Downloading audio level %s lesson %s' % (level, lesson)
+    print urllib.urlretrieve(source_file, filename)
+    print '[+] Saved to ' + filename
+
+def download_pdf(level, lesson, filename=None):
+    source_file = ('http://talktomeinkorean.com/pdf-files/'
+                   'ttmik-l%sl%s.pdf') % (level, lesson)
+        
+    if filename is None:
+        filename = ('ttmik-l%sl%s.pdf') % (level, lesson)
+    
+    # Simple download file
+    # TO DO : Change to http://stackoverflow.com/a/9740603/1198772
+    print '[+] Downloading pdf level %s lesson %s' % (level, lesson)
+    print urllib.urlretrieve(source_file, filename)
+    print '[+] Saved to ' + filename
+
+if __name__ == '__main__':
+    download_pdf(1, 100)
