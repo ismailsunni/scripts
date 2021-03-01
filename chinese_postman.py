56c8c04a00c76d060e1c0cb05e4d766b263152df
Ismail Sunni <imajimatika@gmail.com>
2021-03-01 15:55:12 +0100

    Add get edges.

--- chinese_postman.py
+++ chinese_postman.py
@@ -3,6 +3,21 @@ Implementing chinese postman solver with pure Python.
 """
 
 NOT_CONNECTED = 999
+ZERO_WEIGHT = 0
+
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+class Edge:
+    def __init__(self, node1, node2, weight):
+        self.node1 = node1
+        self.node2 = node2
+        self.weight = weight
+
+    def __str__ (self):
+        return "%s-%s %s" % (self.node1, self.node2, self.weight)
 
 class Graph:
     def __init__(self, directed=False):
@@ -18,24 +33,65 @@ class Graph:
             self.adjacency_matrix.append([NOT_CONNECTED] * len(self.nodes))
             for i in range(len(self.adjacency_matrix)):
                 self.adjacency_matrix[i].append(NOT_CONNECTED)
+            # Self edge is zero
+            self.adjacency_matrix[-1][-1] = ZERO_WEIGHT
         if node2 not in self.nodes:
             self.nodes.append(node2)
             # Add new column to the matrix
             self.adjacency_matrix.append([NOT_CONNECTED] * len(self.nodes))
             for i in range(len(self.adjacency_matrix)):
                 self.adjacency_matrix[i].append(NOT_CONNECTED)
+            # Self edge is zero
+            self.adjacency_matrix[-1][-1] = ZERO_WEIGHT
         self.adjacency_matrix[self.nodes.index(node1)][self.nodes.index(node2)] = weight
         if not self.directed:
             self.adjacency_matrix[self.nodes.index(node2)][self.nodes.index(node1)] = weight
+
+    def get_edges(self):
+        edges = []
+        for i in range(len(self.nodes)):
+            if self.directed:
+                max_iteration = len(self.nodes)
+            else:
+                max_iteration = i
+            for j in range(max_iteration):
+                if self.adjacency_matrix[i][j] != NOT_CONNECTED:
+                    if self.directed:
+                        edges.append(Edge(self.nodes[i], self.nodes[j], self.adjacency_matrix[i][j]))
+                    else:
+                        # sort the nodes if it's not directed graph
+                        first_node = self.nodes[i]
+                        second_node = self.nodes[j]
+                        if first_node < second_node:
+                            edges.append(Edge(first_node, second_node, self.adjacency_matrix[i][j]))
+                        else:
+                            edges.append(Edge(second_node, first_node, self.adjacency_matrix[i][j]))
+        return edges
 
     def print_graph(self):
         print('Directed: %s' % self.directed)
         print('Nodes: %s' % ','.join(self.nodes))
         print('Adjacency matrix:')
+        edges = self.get_edges
         for i in range(len(self.nodes)):
-            for j in range(len(self.nodes)):
+            if self.directed:
+                max_iteration = len(self.nodes)
+            else:
+                max_iteration = i
+            for j in range(max_iteration):
                 if self.adjacency_matrix[i][j] != NOT_CONNECTED:
                     print('%s-%s %s' % (self.nodes[i], self.nodes[j], self.adjacency_matrix[i][j]))
+
+    def get_forward_nodes(self, node):
+        node_index = self.nodes.index(node)
+        print(node_index)
+        forward_node_indexes = self.adjacency_matrix[node_index]
+        forward_nodes = []
+        for i, forward_node_index in enumerate(forward_node_indexes):
+            if forward_node_index != NOT_CONNECTED and forward_node_index != ZERO_WEIGHT:
+                forward_nodes.append(self.nodes[i])
+        return forward_nodes
+
 
 def read_graph(graph_file):
     """Read graph from file.
@@ -49,7 +105,6 @@ def read_graph(graph_file):
     graph = Graph()
     for row in data:
         node1, node2, weight = row.split(',')
-        print('Add edges: ', node1, node2, weight)
         graph.add_edge(node1, node2, int(weight))
 
     return graph
@@ -58,6 +113,11 @@ if __name__ == "__main__":
     graph_file = './simple_graph.csv'
     graph = read_graph(graph_file)
     graph.print_graph()
-
+    node_1 = graph.nodes[0]
+    print(node_1)
+    neighbour_nodes_1 = graph.get_forward_nodes(node_1)
+    print(neighbour_nodes_1)
+    for e in graph.get_edges():
+        print(e)
 
     print('fin')
